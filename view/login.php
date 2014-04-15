df0b62007bbf1486a05c8ca35f4c3bc9acaacf32
Kiwille <kevinalves49@gmail.com>
2014-04-15 21:32:19 +0200

    v140415

--- view/login.php
+++ view/login.php
@@ -6,5 +6,7 @@
     $parse['navbar_login'] = Page::construirePagePartielle('part_navbar_login', $parse);
     $parse['clock_login'] = Page::construirePagePartielle('part_clock', $parse);
     
+    $parse['body_login'] = Page::construirePagePartielle('part_login_accueil', $parse);
+    
     Page::construirePageFinale('part_body_login', $parse, "Uniguerre");
 ?>
