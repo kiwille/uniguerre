c065f02618a03ef1d9e8ac2eeff9e086f69ff77d
mandalorien <mandalorien.wootook@gmail.com>
2014-07-23 08:15:22 +0200

    v23.07.2014 correction ajax
    la function de lecture des fichiers ajax à était simplifié et réduite.

--- view/default/resources/js/ajax.js
+++ view/default/resources/js/ajax.js
@@ -1,60 +1,17 @@
 var dir_controller = "controller/";
 
-function createXHR()
-{
-    if (window.XMLHttpRequest)
-        return new XMLHttpRequest();
-
-    if (window.ActiveXObject) {
-        var names = [
-            "Msxml2.XMLHTTP.6.0",
-            "Msxml2.XMLHTTP.3.0",
-            "Msxml2.XMLHTTP",
-            "Microsoft.XMLHTTP"
-        ];
-        for (var i in names) {
-            try {
-                return new ActiveXObject(names[i]);
-            }
-            catch (e) {
-            }
-        }
-    }
-    window.alert("Votre navigateur ne prend pas en charge l'objet XMLHTTPRequest.");
-    return null; // non supporté
-}
-
-
-
 function ExeRqt(url, querystring, nameid)
 {
-    var xhr = createXHR();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4)
-        {
-            if (xhr.status == 200)
-            {
-                okFunction(xhr.responseText, nameid);
-            }
-            else
-            {
-                errFunction(xhr.status);
-            }
-        }
-    };
-
 	var entities = encodeURIComponent(querystring);
-	
-    xhr.open("GET", dir_controller + url + "?" + entities, true);
-    xhr.send(null);
-}
 
-function errFunction(status)
-{
-    alert("Erreur critique d'exécution ajax:" + status);
-}
-
-function okFunction(resultat, nameid)
-{
-    document.getElementById(nameid).innerHTML = resultat;
+	$.ajax({
+		url: dir_controller + url + "?" + entities,
+		cache: true
+	})
+	.done(function( html ) {
+		$( "#"+nameid+"" ).html( html );
+	})
+	.fail(function( jqXHR, textStatus) {
+		alert("Erreur critique d'exécution ajax: " + textStatus);
+	});
 }
