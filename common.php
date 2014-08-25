79ff6f7f32f8acdc96469b2b325346f5972299f5
Kiwille <kevinalves49@gmail.com>
2014-08-25 22:49:03 +0200

    Fusion langue.php/common.php

--- common.php
+++ common.php
@@ -4,7 +4,33 @@ require_once "tools/includes.php";
 
 $result_access = access_denied();
 
-require_once "json/langue.php";
+//Récupération de la langue choisie
+$tabLangue = array();
+$ToutesLangues = LangueDAO::selectLangues();
+foreach ($ToutesLangues as $langue) {
+    $tabLangue[] = $langue['code'];
+}
+
+if (isset($_POST["langue"]) && !isset($_SESSION['language']) && in_array($_POST["langue"], $tabLangue)) {
+    $_SESSION['language'] = $_POST["langue"];
+} elseif (isset($_POST["langue"]) && isset($_SESSION['language']) && in_array($_POST["langue"], $tabLangue)) {
+    $_SESSION = array();
+    $_SESSION['language'] = $_POST["langue"];
+} elseif (!isset($_POST["langue"]) && !isset($_SESSION['language'])) {
+    $_SESSION = array();
+    $_SESSION['language'] = "FR"; //faire une langue par default
+}
+
+$Alltranslation = TranslationDAO::SQLSelectTranslationParCode($_SESSION['language']);
+foreach ($Alltranslation as $translation) {
+    $lang[$translation['name']] = utf8_encode($translation['value']);
+}
+
+$parse = $lang;
+
+
+
+//-------------------------------------------------------------------------------
 
 $langimg = "";
 
@@ -28,7 +54,7 @@ if (!$result_access) {
     }
 
 
-//Gestion des menus
+    //Gestion des menus
     $menusLogin = MenuDAO::selectMenus(false);
     $menusTriesLogin = array_sort($menusLogin, 'order');
     $menu = "";
