4af701d5dc97d11f47712929bebf9a2fea86c114
Kiwille <kevinalves49@gmail.com>
2014-08-30 00:17:04 +0200

    Réparation partiel du menu par traduction

--- view/default/js/language.js
+++ view/default/js/language.js
@@ -2,37 +2,44 @@
 /*	GESTION DU SYSTEME DE LANGUAGE   */
 ///////////////////////////////////////
 var lang = ['FR', 'EN'];
+
 $(".language").each(function(index)
 {
     $("#language_" + String(lang[index]) + "").click(function() {
-        // Assign handlers immediately after making the request,
-        // and remember the jqxhr object for this request
+        
         $.ajax({
             type: "POST",
             data: "page=" + $("#ajaxpage").val() + "&langue=" + lang[index],
             async: false,
-            url: ""
-        })
-        .done(function(html) {
-            $("#page").html(html);
-            console.log("Traduction page en " + lang[index]);
-        })
-        .fail(function(jqXHR, textStatus) {
-            alert("Erreur critique d'exécution ajax: " + textStatus);
+            url: "",
+            success: function(html, statut) {
+                $("#page").html(html);
+                console.log("Traduction page en " + lang[index]);
+            },
+            error: function(resultat, statut, erreur) {
+                console.log("Erreur traduction page en " + lang[index]);
+            },
+            complete: function(resultat, statut) {
+
+            }
         });
-        
+
         $.ajax({
             type: "POST",
             data: "page=ajax_login_menu&langue=" + lang[index],
             async: false,
-            url: ""
-        })
-        .done(function(html) {
-            $("#navbarLogin").html(html);
-            console.log("Traduction menu en " + lang[index]);
-        })
-        .fail(function(jqXHR, textStatus) {
-            alert("Erreur critique d'exécution ajax: " + textStatus);
+            url: "",
+            success: function(html, statut) {
+                $("#navbarLogin").html(html);
+                console.log("Traduction menu en " + lang[index]);
+            },
+            error: function(resultat, statut, erreur) {
+                console.log("Erreur traduction menu en " + lang[index]);
+            },
+            complete: function(resultat, statut) {
+
+            }
         });
+
     });
 });
