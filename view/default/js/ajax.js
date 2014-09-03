966720ed36a829460fa53cc20d75526cf2db9f40
Kiwille <kevinalves49@gmail.com>
2014-09-03 23:46:40 +0200

    Format du code + retrait du caractère ";" en fin de script

--- view/default/js/ajax.js
+++ view/default/js/ajax.js
@@ -1,16 +1,16 @@
 function ExeRqt(page)
 {
-	$.ajax({
-		url: "",
-		cache: true,
-                type : 'POST',
-                data : "page="+page
-	})
-	.done(function( html ) {
-		$( "#page" ).html( html );
-		console.log("Chargement de page " + page + " terminée.");
-	})
-	.fail(function( jqXHR, textStatus) {
-		console.log("Erreur critique d'exécution ajax: " + textStatus);
-	});
-};
+    $.ajax({
+        url: "",
+        cache: true,
+        type: 'POST',
+        data: "page=" + page
+    })
+            .done(function(html) {
+        $("#page").html(html);
+        console.log("Chargement de page " + page + " terminée.");
+    })
+            .fail(function(jqXHR, textStatus) {
+        console.log("Erreur critique d'exécution ajax: " + textStatus);
+    });
+}
