cc17dba629c0c78a33a126401b42e404a964a62e
Boermans <boermansjo@gmail.com>
2014-09-09 21:28:23 +0200

    Amélioration du soleil
    Nouvelles grandeurs pour le soleil. Ajout d'une lueur+"nuages"
    personaliser pour le soleil.

--- view/default/js/threex.planets.js
+++ view/default/js/threex.planets.js
@@ -4,14 +4,15 @@ THREEx.Planets	= {};
 
 THREEx.Planets.createSun	= function(){
 	var geometry	= new THREE.SphereGeometry(0.5, 32, 32);
-	var texture	= THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'/sun01.jpg');
 	var material	= new THREE.MeshPhongMaterial({
-		map	: texture,
-		bumpMap	: texture,
-		bumpScale: 0.05
+		map		: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'/sun01.jpg'),
+		//bumpMap		: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'/p02.jpg'),
+		bumpScale	: 0.05,
+		//specularMap	: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'/earthspec1k.jpg'),
+		//specular	: new THREE.Color('grey'),
 	});
 	var mesh	= new THREE.Mesh(geometry, material);
-	return mesh;
+	return mesh;	
 };
 
 THREEx.Planets.createStarfield	= function(){
@@ -33,6 +34,64 @@ THREEx.Planets.createPlanet	= function(){
 		bumpScale	: 0.05,
 		//specularMap	: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'/earthspec1k.jpg'),
 		//specular	: new THREE.Color('grey'),
+	});
+	var mesh	= new THREE.Mesh(geometry, material);
+	return mesh;	
+};
+
+THREEx.Planets.createSunDynamics	= function(){
+	// create destination canvas
+	var canvasResult	= document.createElement('canvas');
+	canvasResult.width	= 1024;
+	canvasResult.height	= 512;
+	var contextResult	= canvasResult.getContext('2d');	
+
+	// load earthcloudmap
+	var imageMap	= new Image();
+	imageMap.addEventListener("load", function() {
+		
+		// create dataMap ImageData for earthcloudmap
+		var canvasMap	= document.createElement('canvas');
+		canvasMap.width	= imageMap.width;
+		canvasMap.height= imageMap.height;
+		var contextMap	= canvasMap.getContext('2d');
+		contextMap.drawImage(imageMap, 0, 0);
+		var dataMap	= contextMap.getImageData(0, 0, canvasMap.width, canvasMap.height);
+
+		// load earthcloudmaptrans
+		var imageTrans	= new Image();
+		imageTrans.addEventListener("load", function(){
+			// create dataTrans ImageData for earthcloudmaptrans
+			var canvasTrans		= document.createElement('canvas');
+			canvasTrans.width	= imageTrans.width;
+			canvasTrans.height	= imageTrans.height;
+			var contextTrans	= canvasTrans.getContext('2d');
+			contextTrans.drawImage(imageTrans, 0, 0);
+			var dataTrans		= contextTrans.getImageData(0, 0, canvasTrans.width, canvasTrans.height);
+			// merge dataMap + dataTrans into dataResult
+			var dataResult		= contextMap.createImageData(canvasMap.width, canvasMap.height);
+			for(var y = 0, offset = 0; y < imageMap.height; y++){
+				for(var x = 0; x < imageMap.width; x++, offset += 4){
+					dataResult.data[offset+0]	= dataMap.data[offset+0];
+					dataResult.data[offset+1]	= dataMap.data[offset+1];
+					dataResult.data[offset+2]	= dataMap.data[offset+2];
+					dataResult.data[offset+3]	= 255 - dataTrans.data[offset+0];
+				}
+			}
+			// update texture with result
+			contextResult.putImageData(dataResult,0,0);	
+			material.map.needsUpdate = true;
+		});
+		imageTrans.src	= THREEx.Planets.baseURL+'/cloudmaptrans.jpg';
+	}, false);
+	imageMap.src	= THREEx.Planets.baseURL+'/sun09.jpg';
+
+	var geometry	= new THREE.SphereGeometry(0.51, 32, 32);
+	var material	= new THREE.MeshPhongMaterial({
+		map		: new THREE.Texture(canvasResult),
+		side		: THREE.DoubleSide,
+		transparent	: true,
+		opacity		: 0.8
 	});
 	var mesh	= new THREE.Mesh(geometry, material);
 	return mesh;	
