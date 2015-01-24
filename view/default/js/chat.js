1bfc1f95ae52c3f0572cca300a2df5a0425bc6f9
mandalorien <mandalorien.wootook@gmail.com>
2015-01-24 19:40:01 +0100

    UPDATE : chat.js
    modification du rafraichissement uniquement lorsque a touche Entrer est
    cliquez et non plus rafraichissement toute les secondes  , ca demande
    trop de ressources .

--- view/default/js/chat.js
+++ view/default/js/chat.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-		// actualisation des messages
-		window.setInterval(getMsgs,1000);
+		getMsgs();
+		// window.setInterval(getMsgs,1000);// actualisation des messages
+});
+
+$(document).keyup(function(touche){
+
+    var appui = touche.which || touche.keyCode; // le code est compatible tous navigateurs 
+
+    if(appui == 13){ // si c'est la touche Enter
+		var pathname = window.location.pathname; // Returns path only
+			getMsgs();
+    }
 });
 
 function getMsgs() {
-	// On lance la requête ajax
+	// On lance la requÃªte ajax
 	$.getJSON('controller/ajax_game_chat.php', function(data) {
-		// Si data['error'] renvoi 0, alors ça veut dire que personne n'est en ligne
+		// Si data['error'] renvoi 0, alors Ã§a veut dire que personne n'est en ligne
 		// ce qui n'est pas normal d'ailleurs
 		if(data['error'] == '0') {
+			alert("ok");
 			for (var id in data['list']) {
 				
 				online+=data["list"][id]["msgs"];		
