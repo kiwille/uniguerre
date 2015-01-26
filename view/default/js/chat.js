682caa46a22dc22938be5c14d2455a89268d2a2a
Kiwille <kevinalves49@gmail.com>
2015-01-26 23:56:54 +0100

    fix: rectifie les problèmes de base du chat

--- view/default/js/chat.js
+++ view/default/js/chat.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
-		getMsgs();
-		// window.setInterval(getMsgs,1000);// actualisation des messages
+    getMsgs();
+    // window.setInterval(getMsgs,1000);// actualisation des messages
 });
 
-$(document).keyup(function(touche){
-
+/*$(document).keyup(function(touche) {
     var appui = touche.which || touche.keyCode; // le code est compatible tous navigateurs 
 
-    if(appui == 13){ // si c'est la touche Enter
-		var pathname = window.location.pathname; // Returns path only
-			getMsgs();
+    if (appui === 13) { // si c'est la touche Enter
+        var pathname = window.location.pathname; // Returns path only
+        getMsgs();
     }
-});
+});*/
 
 function getMsgs() {
-	// On lance la requête ajax
-	$.getJSON('controller/ajax_game_chat.php', function(data) {
-		// Si data['error'] renvoi 0, alors ça veut dire que personne n'est en ligne
-		// ce qui n'est pas normal d'ailleurs
-		if(data['error'] == '0') {
-			alert("ok");
-			for (var id in data['list']) {
-				
-				online+=data["list"][id]["msgs"];		
-			}
-			$("#msgs").html(online);
-		}
-	});
+    // On lance la requête ajax
+    $.getJSON('controller/json_game_chat.php', function(data) {
+        var str = "";
+        $.each( data, function( idMess, dataMess ) {
+            str += "<div class='well well-sm' style='margin: 0px;'>";
+            str += dataMess.time + " > " + dataMess.sender + " à " + dataMess.recipients + ": " + dataMess.message;
+            str += "</div>";
+        });
+        $("#msgs").html(str);
+    })
+    .fail(function(jqXHR, textStatus) {
+        alert("Impossible de récuéperer les données du chat : " + textStatus);
+    });
 }
