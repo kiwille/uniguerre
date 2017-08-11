f33d3568eda0347d6ce4ff05a6e87167c2835b7a
ALVES <kevinalves49@gmail.com>
2017-08-11 19:11:35 +0200

    Prépare la partie "Bâtiments" du jeu (structure seulement)

---
+++ classes/StructureResource.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * classe StructureResource
+ */
+class StructureResource {
+    
+    public $id_structure_resource;
+    public $id_structure;
+    public $id_resource;
+    public $cost_base_structure;
+    
+}
--- tools/includes.php
+++ tools/includes.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Pour chaque catégorie, merci de les classer par ordre alphabétique
+ * pour mieux se repérer.
+ */
+
 require_once "constant.php";
 require_once dirname(__DIR__) . "/" . UNIGUERRE_FILE_CONFIG;
 
@@ -22,13 +27,14 @@ require_once UNIGUERRE_DIR_TOOLS . '/array_sort.php';
 /** Model */
 /** -- Classe -- */
 /** ------------ */
+require_once UNIGUERRE_DIR_MODELS . '/Chat.php';
 require_once UNIGUERRE_DIR_MODELS . '/Language.php';
+require_once UNIGUERRE_DIR_MODELS . '/Menu.php';
 require_once UNIGUERRE_DIR_MODELS . '/Planet.php';
 require_once UNIGUERRE_DIR_MODELS . '/PlanetImage.php';
 require_once UNIGUERRE_DIR_MODELS . '/User.php';
 require_once UNIGUERRE_DIR_MODELS . '/Resource.php';
-require_once UNIGUERRE_DIR_MODELS . '/Chat.php';
-require_once UNIGUERRE_DIR_MODELS . '/Menu.php';
+require_once UNIGUERRE_DIR_MODELS . '/Structure.php';
 require_once UNIGUERRE_DIR_MODELS . '/Translation.php';
 /** ------------ */
 require_once UNIGUERRE_DIR_MODELS . '/core/Page.php';
@@ -38,33 +44,39 @@ require_once UNIGUERRE_DIR_MODELS . '/core/DatabaseBackup.php';
 
 /** DAL (Data Access Layer) > DAO (Data Access Object) */
 require_once UNIGUERRE_DIR_DAL . '/daos/DataAccessModel.php';
-require_once UNIGUERRE_DIR_DAL . '/daos/UtilisateurDAO.php';
-require_once UNIGUERRE_DIR_DAL . '/daos/RessourceDAO.php';
+/** ------------ */
+require_once UNIGUERRE_DIR_DAL . '/daos/ChatDAO.php';
 require_once UNIGUERRE_DIR_DAL . '/daos/LangueDAO.php';
 require_once UNIGUERRE_DIR_DAL . '/daos/MenuDAO.php';
-require_once UNIGUERRE_DIR_DAL . '/daos/TranslationDAO.php';
-require_once UNIGUERRE_DIR_DAL . '/daos/ChatDAO.php';
 require_once UNIGUERRE_DIR_DAL . '/daos/PlaneteDAO.php';
 require_once UNIGUERRE_DIR_DAL . '/daos/PlaneteImageDAO.php';
+require_once UNIGUERRE_DIR_DAL . '/daos/UtilisateurDAO.php';
+require_once UNIGUERRE_DIR_DAL . '/daos/RessourceDAO.php';
+require_once UNIGUERRE_DIR_DAL . '/daos/StructureDAO.php';
+require_once UNIGUERRE_DIR_DAL . '/daos/TranslationDAO.php';
 
 /** DAL (Data Access Layer) > REQUESTS (requêtes SQL) */
+/* -- INSERT -- */
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLInsertChat.php';
+/* -- SELECT -- */
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectChat.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectLanguages.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectMenus.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectPlanets.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectPlanetsImages.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectResources.php';
+require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectStructures.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectTranslations.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/SQLSelectUsers.php';
 
 /** DAL (Data Access Layer) > REQUESTS (requêtes SQL) > Tables */
+require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Chat.php';
+require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Languages.php';
+require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Menus.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Planets.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Planets_Images.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Resources.php';
+require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Structures.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Users.php';
-require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Menus.php';
-require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Languages.php';
 require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Translations.php';
-require_once UNIGUERRE_DIR_DAL . '/requests/tables/T_Chat.php';
 
